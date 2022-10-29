bd622bac7707648ab6374918df5ae67f332cae9c
komuro4182 <komuro4182@gmail.com>
2022-10-29 15:06:44 +0900

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by 小室　亮太 on 2022/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
